58019b05455711585e653bd6807b482e1d715eec
Majahenkhosi Dlamini <majahenkhoisdlamini@gmail.com>
2023-02-16 06:20:49 -0800

    Sizes in bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char character;
+	int integer;
+	long longint;
+	long long longlong;
+	float floating;
+
+	printf("Size of a char: %lu bytes(s)\n", sizeof(character);
+	printf("Size of an int: %lu bytes(s)\n", sizeof(integer);
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(longint);
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(longlong);
+	printf("Size of a float: %lu bytes(s)\n", sizeof(floating);
+	
+	return (0);
+}
