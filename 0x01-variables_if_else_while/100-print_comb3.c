c6ab296227af5ddfc3370c92bd450a0dfbef9989
Majahenkhosi Dlamini <majahenkhoisdlamini@gmail.com>
2023-02-17 06:13:20 -0800

    prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 	for (ones = tens + 1; ones <= 9; ones++)
 	{
-	       	putchar(tens + '0');
+	putchar(tens + '0');
 		putchar(ones + '0');
 	if (tens < 8)
 	{
-	       	putchar(',');
+	putchar(',');
 		putchar(' ');
 	}
 	}
