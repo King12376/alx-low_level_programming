5ea4f68f624d85aa961d0f692b62aae2bdd2f606
Majahenkhosi Dlamini <majahenkhoisdlamini@gmail.com>
2023-02-17 05:34:35 -0800

    Lower and upper Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char x;
+char x;
 
-	for (x = 'a'; x <= 'z'; x++)
-	putchar(x);
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
 
-	for (x = 'A'; x <= 'Z'; x++)
-        putchar(x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar(x);
 
-	putchar('\n');
+putchar('\n');
 
 return (0);
 }
