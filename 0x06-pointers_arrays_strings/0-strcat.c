c1dcf7fb9945085c5f9f3a473c3ef5f8385a5aed
Majahenkhosi Dlamini <majahenkhoisdlamini@gmail.com>
2023-03-01 22:52:53 -0800

    removed main

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strncat - concatenates two strings,
  * @dest: destination.
@@ -25,18 +24,4 @@ char *_strncat(char *dest, char *src, int n)
 		count2++;
 	}
 	return (dest);
-}
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
 }
